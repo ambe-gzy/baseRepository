904a0fbe5c6a769b5a573cf8bb7feaf260624952
ambe <13543479929@163.com>
2019-11-12 22:50:38 +0800

    计步软件初步

---
+++ app/src/main/java/cn/zhenye/runfuns/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.zhenye.runfuns;
+
+import androidx.appcompat.app.AppCompatActivity;
+import cn.zhenye.common.cache.ZhenyeCache;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ common/src/main/java/cn/zhenye/common/cache/ZhenyeCache.java
@@ -0,0 +1,10 @@
+package cn.zhenye.common.cache;
+
+import android.util.Log;
+
+public class ZhenyeCache {
+
+    public static void test(){
+        Log.d("aaa","bbb");
+
+    }}
