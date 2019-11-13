f0adc8b257b7f020adc8af7a525096d0b9c63aa1
ambe <13543479929@163.com>
2019-11-13 23:51:34 +0800

    保存

--- app/src/main/java/cn/zhenye/runfuns/MainActivity.java
+++ app/src/main/java/cn/zhenye/runfuns/MainActivity.java
@@ -1,15 +1,27 @@
 package cn.zhenye.runfuns;
 
 import androidx.appcompat.app.AppCompatActivity;
-import cn.zhenye.common.cache.ZhenyeCache;
+import cn.zhenye.base.cache.ZyCacheStorage;
+import cn.zhenye.base.constants.ZyConstants;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ZyCacheStorage.init(getApplicationContext());
 
+    }
+
+    public void btn1(View view) {
+        int time  = 100;
+        ZyCacheStorage.put(ZyConstants.KEY_HOME_LOGIN_TIME,time);
+    }
+
+    public void btn2(View view) {
+        int time = ZyCacheStorage.getInt(ZyConstants.KEY_HOME_LOGIN_TIME,0);
     }
 }
