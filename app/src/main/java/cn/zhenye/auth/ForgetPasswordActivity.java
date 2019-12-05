9b80fa65ab0e85284334c43c37364072bc98c6a1
zhenye <gongzhenye@yolonet.io>
2019-12-05 14:24:17 +0800

    新增找回密码页面

---
+++ app/src/main/java/cn/zhenye/auth/ForgetPasswordActivity.java
@@ -0,0 +1,16 @@
+package cn.zhenye.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.zhenye.runfuns.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
