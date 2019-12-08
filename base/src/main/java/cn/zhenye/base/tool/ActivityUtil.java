ecb12c0ab7176bacc671d326fe1fcc2c4566bc59
ambe <13543479929@163.com>
2019-12-09 00:48:32 +0800

    新增工具类 activityUtil

---
+++ base/src/main/java/cn/zhenye/base/tool/ActivityUtil.java
@@ -0,0 +1,47 @@
+package cn.zhenye.base.tool;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+public class ActivityUtil {
+
+    public static boolean safeStartActivityWithIntentClass(@NonNull Context context, @NonNull Class className) {
+        return safeStartActivityWithIntent(context, new Intent(context, className));
+    }
+
+    public static boolean safeStartActivityWithIntent(@NonNull Context context, @NonNull Intent intent) {
+        try {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean safeStartActivityWithIntent(@NonNull Activity activity, @NonNull Intent intent) {
+        try {
+            activity.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean safeStartActivityForResultWithIntent(@NonNull Activity activity, @NonNull Intent intent,
+                                                               int requestCode) {
+        try {
+            activity.startActivityForResult(intent, requestCode);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
