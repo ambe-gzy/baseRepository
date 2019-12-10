52d5b390c534bdd16a34aee735337dd8b0821f00
zhenye <gongzhenye@yolonet.io>
2019-12-10 18:54:03 +0800

    新增基础抽象类FullScreenActivity

---
+++ base/src/main/java/cn/zhenye/base/activity/FullScreenActivity.java
@@ -0,0 +1,91 @@
+package cn.zhenye.base.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import cn.zhenye.base.R;
+import cn.zhenye.base.tool.StatusbarUtil;
+
+public abstract class FullScreenActivity extends AppCompatActivity {
+
+    private FrameLayout mWindowBackground;
+    //布局容器
+    private ViewGroup mContentWrapperView;
+    //布局完成的容器
+    private View mContentView = null;
+    //toolbar
+    private Toolbar mToolbar;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_fullscreen);
+        initView();
+        initWindow();
+
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        LayoutInflater inflater = LayoutInflater.from(FullScreenActivity.this);
+        mContentView = inflater.inflate(layoutResID, mContentWrapperView, true);
+    }
+
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return mContentView.findViewById(id);
+    }
+
+    private void initView() {
+        mWindowBackground = super.findViewById(R.id.fl_window_background);
+        mContentWrapperView = super.findViewById(R.id.fl_content_wrapper_view);
+        mToolbar = super.findViewById(R.id.tl_toolbar);
+    }
+
+    private void initWindow() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            StatusbarUtil.setWindowFullScreenWithStatusBar(getWindow(), true);
+            //适配状态栏高度
+            LinearLayout mWindowView = super.findViewById(R.id.ll_toolbar_and_wrapper_view);
+            fitStatusBar(mWindowView);
+        }
+
+
+    }
+
+
+    private void fitStatusBar(View view) {
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) view.getLayoutParams();
+        lp.topMargin = StatusbarUtil.getStatusBarHeight(getApplicationContext());
+        view.setLayoutParams(lp);
+    }
+
+    /**
+     * 用于设置背景颜色
+     *
+     * @return
+     */
+    public FrameLayout getWindowBackground() {
+        return mWindowBackground;
+    }
+
+    /**
+     * 用于设置toolbar相关的属性
+     *
+     * @return
+     */
+    public Toolbar getToolbar() {
+        return mToolbar;
+    }
+
+}
---
+++ base/src/main/java/cn/zhenye/base/tool/StatusbarUtil.java
@@ -0,0 +1,76 @@
+package cn.zhenye.base.tool;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.ColorInt;
+
+public class StatusbarUtil {
+    private static int statusBarHeight = -1;
+
+    /**
+     * 设置状态栏背景颜色
+     *
+     * @param window
+     * @param color
+     */
+    public static void setStatusBarBackgroundColor(Window window, @ColorInt int color) {
+        window.setStatusBarColor(color);
+    }
+
+    /**
+     * 设置状态栏字体是否为黑色，只允许6.0以上版本修改
+     */
+    public static void setStatusBarTextColor(Window window,boolean dark){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decorView = window.getDecorView();
+            decorView.setSystemUiVisibility(dark ? View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR : 0);
+        }
+    }
+
+    /**
+     * 设置是否全屏显示内容（会与内容发生重叠，需要进行适配)只允许6.0以上使用全屏，防止字体颜色问题。
+     */
+    public static void setWindowFullScreenWithStatusBar(Window window, boolean fullscreen){
+        if (fullscreen){
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN|WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        }else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN|WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        }
+    }
+
+    /**
+     * 将dp转化为px
+     * @param context
+     * @param dp
+     * @return
+     */
+    public static int dp2px(Context context, float dp) {
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+        return (int) px;
+    }
+
+    /**
+     * 获取statusbar高度
+     */
+    public static int getStatusBarHeight(Context context) {
+        if (statusBarHeight != -1) {
+            return statusBarHeight;
+        }
+
+        //获取status_bar_height资源的ID
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            //根据资源ID获取响应的尺寸值
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
+    }
+
+
+
+}
