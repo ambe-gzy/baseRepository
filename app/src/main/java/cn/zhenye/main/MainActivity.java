cc80479b73e50ae5483f5c591d62e8c821ddca84
zhenye <gongzhenye@yolonet.io>
2019-12-18 19:26:31 +0800

    保存自定义布局

--- app/src/main/java/cn/zhenye/main/MainActivity.java
+++ app/src/main/java/cn/zhenye/main/MainActivity.java
@@ -9,8 +9,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import com.umeng.analytics.MobclickAgent;
-
 
 public class MainActivity extends ZyCommonActivity implements View.OnClickListener {
     private LinearLayout mVoiceReverseBtn;
---
+++ base/src/main/java/cn/zhenye/base/view/RecyclerNormalItem.java
@@ -0,0 +1,105 @@
+package cn.zhenye.base.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.DimenRes;
+import androidx.annotation.IntegerRes;
+import androidx.annotation.Nullable;
+
+import cn.zhenye.base.R;
+
+public class RecyclerNormalItem extends LinearLayout {
+    private ImageView mIvLeftIcon;
+    private TextView mTvMessage;
+    private ImageView mIvRightIcon;
+
+    @ColorInt
+    private int mMessageColor;
+    private int mMessageSize;
+    private String mMessage;
+    private Drawable mLeftIconDrawable;
+    private Drawable mRightIconDrawable;
+
+
+    public RecyclerNormalItem(Context context) {
+        super(context);
+    }
+
+    public RecyclerNormalItem(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView(context,attrs);
+    }
+
+    public RecyclerNormalItem(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RecyclerNormalItem(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initView(Context context, AttributeSet attr){
+        View.inflate(context, R.layout.item_recycler_normal, RecyclerNormalItem.this);
+        mIvLeftIcon = this.findViewById(R.id.iv_icon_left);
+        mTvMessage = this.findViewById(R.id.tv_message);
+        mIvRightIcon = this.findViewById(R.id.iv_icon_right);
+
+        TypedArray ta = context.obtainStyledAttributes(attr,R.styleable.RecyclerNormalItemStyle,0,0);
+        mMessage = ta.getString(R.styleable.RecyclerNormalItemStyle_message);
+        mMessageColor = ta.getColor(R.styleable.RecyclerNormalItemStyle_messageColor, Color.BLACK);
+        mMessageSize = ta.getDimensionPixelSize(R.styleable.RecyclerNormalItemStyle_messageSize, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX,32,context.getResources().getDisplayMetrics()));
+        mLeftIconDrawable = ta.getDrawable(R.styleable.RecyclerNormalItemStyle_leftIcon);
+        mRightIconDrawable = ta.getDrawable(R.styleable.RecyclerNormalItemStyle_rightIcon);
+        ta.recycle();
+
+        initMessage(mTvMessage,mMessageColor,mMessageSize,mMessage);
+        initLeftIcon(mIvLeftIcon,mLeftIconDrawable,context);
+        initRightIcon(mIvRightIcon,mRightIconDrawable,context);
+    }
+
+    private void initMessage(TextView textView,int textColor,long textSize,String text){
+        textView.setTextColor(textColor);
+        textView.setTextSize(textSize);
+        textView.setText(text);
+    }
+
+    private void initLeftIcon(ImageView imageView, Drawable drawable,Context context){
+        if (drawable == null){
+            drawable = context.getResources().getDrawable(R.mipmap.ic_close);
+        }
+        imageView.setImageDrawable(drawable);
+    }
+
+    private void initRightIcon(ImageView imageView, Drawable drawable,Context context){
+        if (drawable == null){
+            drawable = context.getResources().getDrawable(R.mipmap.ic_page_in);
+        }
+        imageView.setImageDrawable(drawable);
+    }
+
+    public TextView getTvMessage(){
+        return mTvMessage;
+    }
+
+    public ImageView getmIvLeftIcon(){
+        return mIvLeftIcon;
+    }
+
+    public ImageView getmIvRightIcon(){
+        return mIvRightIcon;
+    }
+
+
+}
