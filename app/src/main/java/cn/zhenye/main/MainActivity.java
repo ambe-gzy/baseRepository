f7135aa129f0ed634c7be58a3d4262f8e8f97bd4
ambe <13543479929@163.com>
2019-12-19 00:23:20 +0800

    语音倒放挑战主页初步完成

--- app/src/main/java/cn/zhenye/main/MainActivity.java
+++ app/src/main/java/cn/zhenye/main/MainActivity.java
@@ -4,17 +4,17 @@ import cn.zhenye.appcommon.ZyCommonActivity;
 import cn.zhenye.base.tool.ActivityUtil;
 import cn.zhenye.base.tool.StatusbarUtil;
 import cn.zhenye.voicereverse.VoiceFileActivity;
-import cn.zhenye.voicereverse.VoiceReverseActivity;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-
-import com.umeng.analytics.MobclickAgent;
 
 
 public class MainActivity extends ZyCommonActivity implements View.OnClickListener {
     private LinearLayout mVoiceReverseBtn;
+    private ImageView mIvGuide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,12 @@ public class MainActivity extends ZyCommonActivity implements View.OnClickListen
     private void initUI(){
         StatusbarUtil.setStatusBarTextColor(getWindow(),true);
         mVoiceReverseBtn = findViewById(R.id.ll_main_voice_reverse);
+        mIvGuide = findViewById(R.id.iv_main_guide);
+        mIvGuide.setColorFilter(getResources().getColor(R.color.color_3C3885), PorterDuff.Mode.SRC_IN);
+
+
         mVoiceReverseBtn.setOnClickListener(this);
+        mIvGuide.setOnClickListener(this);
     }
 
 
@@ -36,6 +41,10 @@ public class MainActivity extends ZyCommonActivity implements View.OnClickListen
         switch (id){
             case R.id.ll_main_voice_reverse:
                 ActivityUtil.safeStartActivityWithActivity(this, VoiceFileActivity.class);
+                break;
+            case R.id.iv_main_guide:
+                //todo 显示如何玩；
+                break;
         }
     }
 }
