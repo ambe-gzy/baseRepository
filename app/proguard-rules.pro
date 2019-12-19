aaba2aacdf879f5265faf33703c35b41da22f10b
ambe <13543479929@163.com>
2019-12-19 23:43:37 +0800

    开屏广告初步

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,62 @@ public static final int *;
 }
 #混淆（Obfuscation）：默认开启，增大反编译难度，类和类成员会被随机命名，除非用keep保护。
 #-dontobfuscate 关闭混淆
+
+# Demo工程里用到了AQuery库，因此需要添加下面的配置
+# 请开发者根据自己实际情况给第三方库的添加相应的混淆设置
+-dontwarn com.androidquery.**
+-keep class com.androidquery.** { *;}
+
+-dontwarn tv.danmaku.**
+-keep class tv.danmaku.** { *;}
+
+-dontwarn androidx.**
+
+# 如果使用了tbs版本的sdk需要进行以下配置
+-keep class com.tencent.smtt.** { *; }
+-dontwarn dalvik.**
+-dontwarn com.tencent.smtt.**
+
+# 如果使用了微信OpenSDK，需要添加如下配置
+-keep class com.tencent.mm.opensdk.** {
+    *;
+}
+
+-keep class com.tencent.wxop.** {
+    *;
+}
+
+-keep class com.tencent.mm.sdk.** {
+    *;
+}
+
+# 如果接入了Bugly，需要添加如下配置
+-dontwarn com.tencent.bugly.**
+-keep public class com.tencent.bugly.**{*;}
+
+# 穿山甲
+-keep class com.bytedance.sdk.openadsdk.** { *; }
+-keep class com.androidquery.callback.** {*;}
+-keep public interface com.bytedance.sdk.openadsdk.downloadnew.** {*;}
+-keep class com.ss.sys.ces.* {*;}
+-dontwarn com.ss.android.socialbase.downloader.impls.**
+-dontwarn com.ss.android.crash.log.**
+
+# 百度
+-keep class com.baidu.mobads.*.** { *; }
+-keep class com.baidu.mobad.*.** { *; }
+
+# MTG
+-keepattributes Signature
+-keepattributes *Annotation*
+-keep class com.mintegral.** {*; }
+-keep interface com.mintegral.** {*; }
+-keep class android.support.v4.** { *; }
+-dontwarn com.mintegral.**
+-keep class **.R$* { public static final int mintegral*; }
+-keep class com.alphab.** {*; }
+-keep interface com.alphab.** {*; }
+
+# 快手
+-dontwarn com.ksad.download.**
+-dontwarn com.kwad.sdk.**
--- app/src/main/java/cn/zhenye/appcommon/SplashActivity.java
+++ app/src/main/java/cn/zhenye/appcommon/SplashActivity.java
@@ -5,9 +5,32 @@ import cn.zhenye.base.tool.Daemon;
 import cn.zhenye.main.MainActivity;
 import cn.zhenye.main.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.KeyEvent;
 
-public class SplashActivity extends ZyCommonActivity {
+import com.qq.e.ads.splash.SplashADListener;
+import com.qq.e.comm.util.AdError;
+
+public class SplashActivity extends ZyCommonActivity implements SplashADListener {
+
+    private static final String SKIP_TEXT = "点击跳过 %d";
+    public boolean canJump = false;
+    private boolean needStartDemoList = true;
+    /**
+     * 为防止无广告时造成视觉上类似于"闪退"的情况，设定无广告时页面跳转根据需要延迟一定时间，demo
+     * 给出的延时逻辑是从拉取广告开始算开屏最少持续多久，仅供参考，开发者可自定义延时逻辑，如果开发者采用demo
+     * 中给出的延时逻辑，也建议开发者考虑自定义minSplashTimeWhenNoAD的值（单位ms）
+     **/
+    private int minSplashTimeWhenNoAD = 2000;
+    /**
+     * 记录拉取广告的时间
+     */
+    private long fetchSplashADTime = 0;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +44,107 @@ public class SplashActivity extends ZyCommonActivity {
         },1000);
     }
 
-    //闪屏页选择activity进入
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (canJump) {
+            next();
+        }
+        canJump = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        canJump = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    /**
+     * 开屏页一定要禁止用户对返回按钮的控制，否则将可能导致用户手动退出了App而广告无法正常曝光和计费
+     * */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * 设置一个变量来控制当前开屏页面是否可以跳转，当开屏广告为普链类广告时，点击会打开一个广告落地页，此时开发者还不能打开自己的App主页。当从广告落地页返回以后，
+     * 才可以跳转到开发者自己的App主页；当开屏广告是App类广告时只会下载App。
+     */
+    private void next() {
+        if (canJump) {
+            if (needStartDemoList) {
+                this.startActivity(new Intent(this, DemoListActivity.class));
+            }
+            this.finish();
+        } else {
+            canJump = true;
+        }
+    }
+
+        //闪屏页选择activity进入
     private void switchActivity(){
         ActivityUtil.safeStartActivityWithIntentClass(getApplicationContext(), MainActivity.class);
         finish();
+    }
+
+    @Override
+    public void onADDismissed() {
+
+    }
+
+    @Override
+    public void onNoAD(AdError error) {
+        Log.i(
+                "AD_DEMO",
+                String.format("LoadSplashADFail, eCode=%d, errorMsg=%s", error.getErrorCode(),
+                        error.getErrorMsg()));
+        /**
+         * 为防止无广告时造成视觉上类似于"闪退"的情况，设定无广告时页面跳转根据需要延迟一定时间，demo
+         * 给出的延时逻辑是从拉取广告开始算开屏最少持续多久，仅供参考，开发者可自定义延时逻辑，如果开发者采用demo
+         * 中给出的延时逻辑，也建议开发者考虑自定义minSplashTimeWhenNoAD的值
+         **/
+        long alreadyDelayMills = System.currentTimeMillis() - fetchSplashADTime;//从拉广告开始到onNoAD已经消耗了多少时间
+        long shouldDelayMills = alreadyDelayMills > minSplashTimeWhenNoAD ? 0 : minSplashTimeWhenNoAD
+                - alreadyDelayMills;//为防止加载广告失败后立刻跳离开屏可能造成的视觉上类似于"闪退"的情况，根据设置的minSplashTimeWhenNoAD
+        // 计算出还需要延时多久
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (needStartDemoList) {
+                    SplashActivity.this.startActivity(new Intent(SplashActivity.this, DemoListActivity.class));
+                }
+                SplashActivity.this.finish();
+            }
+        }, shouldDelayMills);
+    }
+
+    @Override
+    public void onADPresent() {
+
+    }
+
+    @Override
+    public void onADClicked() {
+
+    }
+
+    @Override
+    public void onADTick(long l) {
+
+    }
+
+    @Override
+    public void onADExposure() {
+
     }
 }
