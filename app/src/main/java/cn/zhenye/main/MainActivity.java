4440e65589d559e4f148f2ccbfffb7a64f06c641
ambe <13543479929@163.com>
2019-12-21 11:31:10 +0800

    navigationbar编写完成

--- app/src/main/java/cn/zhenye/main/MainActivity.java
+++ app/src/main/java/cn/zhenye/main/MainActivity.java
@@ -5,35 +5,79 @@ import cn.zhenye.base.tool.ActivityUtil;
 import cn.zhenye.base.tool.StatusbarUtil;
 import cn.zhenye.voicereverse.VoiceFileActivity;
 
+import android.graphics.ColorFilter;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.umeng.analytics.MobclickAgent;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
 
 public class MainActivity extends ZyCommonActivity implements View.OnClickListener {
     private LinearLayout mVoiceReverseBtn;
     private ImageView mIvGuide;
+    private BottomNavigationBar mNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initUI();
+        initNavigationBar();
     }
 
     private void initUI(){
         StatusbarUtil.setStatusBarTextColor(getWindow(),true);
         mVoiceReverseBtn = findViewById(R.id.ll_main_voice_reverse);
         mIvGuide = findViewById(R.id.iv_main_guide);
-        mIvGuide.setColorFilter(getResources().getColor(R.color.color_3C3885), PorterDuff.Mode.SRC_IN);
+        mNavigationBar = findViewById(R.id.nvb_activity_main_navigation_bar);
 
+        mIvGuide.setColorFilter(getResources().getColor(R.color.color_3C3885), PorterDuff.Mode.SRC_IN);
 
         mVoiceReverseBtn.setOnClickListener(this);
         mIvGuide.setOnClickListener(this);
+    }
+
+    private void initNavigationBar() {
+        final Drawable gameIcon = getResources().getDrawable(R.mipmap.ic_tab_game);
+        final Drawable coinIcon = getResources().getDrawable(R.mipmap.ic_tab_coin);
+        BottomNavigationItem itemGame  = new BottomNavigationItem(gameIcon,R.string.activity_main_navigation_game)
+                .setActiveColorResource(R.color.white);//item被选中时BottomNavigationBar的背景颜色
+        BottomNavigationItem itemCoin  = new BottomNavigationItem(coinIcon,R.string.activity_main_navigation_coin)
+                .setActiveColorResource(R.color.white);//item被选中时BottomNavigationBar的背景颜色
+
+        mNavigationBar.setMode(BottomNavigationBar.MODE_FIXED).
+                setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE).
+                setAnimationDuration(250).//动画效果持续时长
+                setActiveColor(R.color.white).//BottomNavigationBar背景顏色
+                setBarBackgroundColor(R.color.color_3C3885).//tittle/icon选中颜色
+                setInActiveColor(R.color.color_A6A0C6);//tittle/icon未选中颜色
+        mNavigationBar.addItem(itemGame).addItem(itemCoin).initialise();//添加图标
+        mNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                switch (position){
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+                switch (position){
+
+                }
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+
+            }
+        });
+        mNavigationBar.selectTab(0);
     }
 
 
@@ -50,15 +94,4 @@ public class MainActivity extends ZyCommonActivity implements View.OnClickListen
         }
     }
 
-    @Override
-    protected void onResume() {
-        MobclickAgent.onResume(this);
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        MobclickAgent.onPause(this);
-        super.onPause();
-    }
 }
