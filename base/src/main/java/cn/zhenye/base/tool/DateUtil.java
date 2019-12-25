4f806f15adc30e11bfc390820cb5757eff7059df
zhenye <gongzhenye@yolonet.io>
2019-12-25 18:24:18 +0800

    根据时间戳判断是否同一天

--- base/src/main/java/cn/zhenye/base/tool/DateUtil.java
+++ base/src/main/java/cn/zhenye/base/tool/DateUtil.java
@@ -2,6 +2,7 @@ package cn.zhenye.base.tool;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
@@ -56,5 +57,67 @@ public class DateUtil {
             e.printStackTrace();
         }
         return date.getTime();
+    }
+
+    /**
+     * 两个时间戳是否是同一天 时间戳是long型的（11或者13）
+     * @param currentTime
+     * @param lastTime
+     * @return
+     */
+    public static boolean isSameData(String currentTime,String lastTime) {
+        try {
+            Calendar nowCal = Calendar.getInstance();
+            Calendar dataCal = Calendar.getInstance();
+            SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
+            SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
+            Long nowLong = Long.valueOf(currentTime);
+            Long dataLong = Long.valueOf(lastTime);
+            String data1 = df1.format(nowLong);
+            String data2 = df2.format(dataLong);
+            java.util.Date now = df1.parse(data1);
+            java.util.Date date = df2.parse(data2);
+            nowCal.setTime(now);
+            dataCal.setTime(date);
+            return isSameDay(nowCal, dataCal);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 两个时间戳是否是同一天 时间戳是long型的（11或者13）
+     * @param currentTime
+     * @param lastTime
+     * @return
+     */
+    public static boolean isSameData(long currentTime,long lastTime) {
+        try {
+            Calendar nowCal = Calendar.getInstance();
+            Calendar dataCal = Calendar.getInstance();
+            SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
+            SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
+            String data1 = df1.format(currentTime);
+            String data2 = df2.format(lastTime);
+            java.util.Date now = df1.parse(data1);
+            java.util.Date date = df2.parse(data2);
+            nowCal.setTime(now);
+            dataCal.setTime(date);
+            return isSameDay(nowCal, dataCal);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static boolean isSameDay(Calendar cal1, Calendar cal2) {
+        if(cal1 != null && cal2 != null) {
+            return cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA)
+                    && cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+                    && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
+        } else {
+            return false;
+        }
     }
 }
