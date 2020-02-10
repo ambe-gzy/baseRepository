f562e2fa85e5fe2db5e2fbcd99540373c14cfab3
zhenye <gongzhenye@yolonet.io>
2020-02-10 14:21:32 +0800

    修改隐式政策地址

--- common/src/main/java/cn/zhenye/common/constants/HttpUrlConstants.java
+++ common/src/main/java/cn/zhenye/common/constants/HttpUrlConstants.java
@@ -2,7 +2,7 @@ package cn.zhenye.common.constants;
 
 public class HttpUrlConstants {
 
-    public static final String HOST_DEFAULT = "120.79.15.35";
+    public static final String HOST_DEFAULT = "http://120.79.15.35";
 
     //隐私政策URL
     public static final String PRIVACY_POLICY_URL = HOST_DEFAULT + "/voicereverse/index.html";
