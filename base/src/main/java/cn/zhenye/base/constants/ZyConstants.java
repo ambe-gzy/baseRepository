169b893efd0fd0e092e25c13973559e51122fb30
zhenye <gongzhenye@yolonet.io>
2020-03-25 20:56:18 +0800

    创建文件

--- base/src/main/java/cn/zhenye/base/constants/ZyConstants.java
+++ base/src/main/java/cn/zhenye/base/constants/ZyConstants.java
@@ -7,4 +7,6 @@ public class ZyConstants {
     public static final String KEY_SP_ZY_COMMON = "sp_key_zy_common_";
 
     public static final String KEY_HOME_LOGIN_TIME = KEY_SP_ZY_BASE +"home_enter_time";
+
+    public static final String KEY_SP_ZY_AD = KEY_SP_ZY_COMMON + "_ad";
 }
