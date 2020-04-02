78badf7c6a1a5ae7fa66e83e3e4df9ae1ddcac53
zhenye <gongzhenye@yolonet.io>
2020-04-02 15:30:33 +0800

    百川电商sdk接入初步

---
+++ baichuan/src/main/java/cn/zhenye/baichuan/test/BaiChuanTestActivity.java
@@ -0,0 +1,16 @@
+package cn.zhenye.baichuan.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.zhenye.baichuan.R;
+
+public class BaiChuanTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_chuan_test);
+    }
+}
